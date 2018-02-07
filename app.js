e63fba5efe1f91704ec83c36b2b7c1983a86f147
Alex Caroselli <alexandersylvia9@msn.com>
2018-02-06 20:46:27 -0700

    first version

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
-var answerButton= document.getElementById('answerButton');
+var answerButton= document.getElementById('answer-button');
+
+
+document.getElementById('inputValue').onkeypress = "if(event.keyCode == 13)document.getElementById('answer-button').click()"
 
 answerButton.onclick = revealAnswer;
 var firstNum =  Math.floor(Math.random() *13)
 var secondNum = Math.floor(Math.random()*13);
 document.getElementById('times').innerHTML = firstNum + " x " + secondNum;
 document.getElementById('answer').innerHTML= firstNum*secondNum;
+var count =0;
+
 function revealAnswer(){
-    document.getElementById('flipper1').classList.toggle('flipped');
-    return false;
+   if(document.getElementById("inputValue").value== firstNum*secondNum){
+   document.getElementById('flipper1').classList.toggle('flipped');
+   document.getElementById("correct-answer").play();
+   return false;
+   count++;
+    }
+   else if(document.getElementById("inputValue").value!= firstNum*secondNum){
+   document.getElementById("wrong-answer").play();
+   document.getElementById('times').innerHTML = firstNum + " x " + secondNum;
+   document.getElementById('answer').innerHTML= firstNum*secondNum;
+   }
 }
-        
-        
+
+ 
+ 
+
+   
+         
         
         
         
